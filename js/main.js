aa9e6505c1cff63c8c5c22a6c66a71d9f15cb9b0
yasmin <yasmintolba227@gmail.com>
2023-12-30 14:43:16 +0200

    header design

--- js/main.js
+++ js/main.js
@@ -1,2 +1,10 @@
 // wow 
-new WOW().init();
+// new WOW().init();
+// toggle active class on header
+let menuBtn = document.querySelectorAll('.list-links .nav-item')
+menuBtn.forEach((li) => {
+    li.addEventListener("click", (e) => {
+        menuBtn.forEach((el) => el.classList.remove("active"));
+        li.classList.add("active");
+    });
+});
